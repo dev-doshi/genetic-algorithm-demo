a535ab34e6b047c4397c3ccb40258a6efb1e1b50
dev-doshi <devdoshi@hotmail.com>
2023-07-07 00:20:02 +0200

    refactor code and improve entity movement

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 import math
+import time
 
 # Initialize pygame
 pygame.init()
@@ -15,68 +16,96 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 # Title and icon
 pygame.display.set_caption("Maze Solver")
 
+last_time = time.time()
+deltatime = 0
+time_since_last_generation = 0
+
+class Labyrinth:
+    def draw(self):
+        # Line on the top
+        pygame.draw.line(screen, (0, 0, 0), (10, 10), (SCREEN_WIDTH - 10, 10), 20)
+        # Line on the bottom
+        pygame.draw.line(screen, (0, 0, 0), (10, SCREEN_HEIGHT - 10), (SCREEN_WIDTH - 10, SCREEN_HEIGHT - 10), 20)
+        # Line on the left
+        pygame.draw.line(screen, (0, 0, 0), (10, 10), (10, SCREEN_HEIGHT - 10), 20)
+        # Line on the right
+        pygame.draw.line(screen, (0, 0, 0), (SCREEN_WIDTH - 10, 10), (SCREEN_WIDTH - 10, SCREEN_HEIGHT - 10), 20)
+        # Vertical line in the middle
+        pygame.draw.line(screen, (0, 0, 0), (SCREEN_WIDTH / 2, 100), (SCREEN_WIDTH / 2, SCREEN_HEIGHT - 10), 20)
+        # Goal on the bottom right
+        pygame.draw.rect(screen, (0, 255, 0), (SCREEN_WIDTH - 60, SCREEN_HEIGHT - 60, 19, 19))
+
+    def willCollide(self, x, y, dx, dy):
+        if x + dx < 0 or x + dx > SCREEN_WIDTH - 20 or y + dy < 0 or y + dy > SCREEN_HEIGHT - 20 or (
+                x + dx > SCREEN_WIDTH / 2 - 20 and y + dy > 100 and x + dx < SCREEN_WIDTH / 2 + 20):
+            return True
+        else:
+            return False
+
 # The entity class
 class Entity:
-    def __init__(self, x, y, color, fitness=0, active=True, steps=[]):
+    def __init__(self, x, y, color, fitness=0, active=True, steps=None, wall_touches=None):
+        if wall_touches is None:
+            wall_touches = []
+        if steps is None:
+            steps = []
+        else:
+            for step in steps:
+                x += step[0]
+                y += step[1]
+
         self.x = x
         self.y = y
         self.color = color
         self.fitness = fitness
         self.active = active
         self.steps = steps
+        self.wall_touches = wall_touches
 
     def draw(self):
         pygame.draw.rect(screen, self.color, (self.x, self.y, 19, 19))
 
-    def calculateFitness(self, touchedGoal, touchedWall):
-        # Give bonus depending on how many steps it took to reach the goal
-        # Give bonus for getting more close to the goal
-        # Give penalty depending on how early it touched the wall
-        # Give minus points for every step
+    def calculateFitness(self):
+        STEPCOST = 0.1
+        DISTANCECOST = 0.2
+        WALLCOST = 0.4
+
+        distanceToGoal = math.sqrt((SCREEN_WIDTH - 60 - self.x) ** 2 + (SCREEN_HEIGHT - 60 - self.y) ** 2)
+
+        # Get the distance to the goal when the entity touched a wall
+        for touch in self.wall_touches:
+            self.fitness += math.sqrt((SCREEN_WIDTH - 60 - (self.x + touch[0])) ** 2 + (SCREEN_HEIGHT - 60 - (self.y + touch[1])) ** 2)
+
+        print(self.fitness)
 
     def move(self, dx, dy):
         if self.active:
-            self.calculateFitness(False, False)
             # Check if the entity is not going out to the wall
-            if not (
-                    self.x + dx < 0 or self.x + dx > SCREEN_WIDTH - 20 or self.y + dy < 0 or self.y + dy > SCREEN_HEIGHT - 20 or (
-                    self.x + dx > SCREEN_WIDTH / 2 - 20 and self.y + dy > 100 and self.x < SCREEN_WIDTH / 2)):
-                # Check if it touches a wall
-                if self.x + dx < 10 or self.x + dx > SCREEN_WIDTH - 60 or self.y + dy < 10 or self.y + dy > SCREEN_HEIGHT - 30:
-                    self.active = False
-                    self.calculateFitness(False, True)
-                elif self.x + dx > SCREEN_WIDTH - 60 and self.y + dy > SCREEN_HEIGHT - 60:
-                    self.active = False
-                    self.calculateFitness(True, False)
-                else:
-                    self.x += dx
-                    self.y += dy
-                    self.steps.append((dx, dy))
+            labyrinth = Labyrinth()
+            if not labyrinth.willCollide(self.x, self.y, dx, dy):
+                self.x += dx
+                self.y += dy
+                self.steps.append((dx, dy))
             else:
-                self.active = False
-                self.calculateFitness(False, True)
+                self.wall_touches.append((dx, dy))
+                print(len(self.wall_touches))
+        else:
+            self.calculateFitness()
 
-entities = []
-# Place entities on start position (bottom left)
-for i in range(10000):
-    entities.append(Entity(60, SCREEN_HEIGHT - 60, (0, 0, 255)))
+def updateScreen(entities):
+    global deltatime
+    global last_time
+    global time_since_last_generation
 
-# Main loop
-while True:
+    deltatime = time.time() - last_time
+    last_time = time.time()
+    time_since_last_generation += deltatime
+
     screen.fill((255, 255, 255))
 
-    # Line on the top
-    pygame.draw.line(screen, (0, 0, 0), (10, 10), (SCREEN_WIDTH-10, 10), 5)
-    #Line on the bottom
-    pygame.draw.line(screen, (0, 0, 0), (10, SCREEN_HEIGHT-10), (SCREEN_WIDTH-10, SCREEN_HEIGHT-10), 5)
-    #Line on the left
-    pygame.draw.line(screen, (0, 0, 0), (10, 10), (10, SCREEN_HEIGHT-10), 5)
-    #Line on the right
-    pygame.draw.line(screen, (0, 0, 0), (SCREEN_WIDTH-10, 10), (SCREEN_WIDTH-10, SCREEN_HEIGHT-10), 5)
-    # Vertical line in the middle
-    pygame.draw.line(screen, (0, 0, 0), (SCREEN_WIDTH / 2, 100), (SCREEN_WIDTH / 2, SCREEN_HEIGHT-10), 5)
-    # Goal on the bottom right
-    pygame.draw.rect(screen, (0, 255, 0), (SCREEN_WIDTH - 60, SCREEN_HEIGHT - 60, 19, 19))
+    # Draw the labyrinth
+    labyrinth = Labyrinth()
+    labyrinth.draw()
 
     # Place the entities
     for entity in entities:
@@ -84,15 +113,10 @@ while True:
 
     # Set colors for entities based on their active state
     for entity in entities:
-        print(entity.fitness)
-        entity.color = (0,  entity.fitness/100000, 0)
-
-    # Move every entity randomly
-    for entity in entities:
-        xMove = random.randint(-10, 10)
-        yMove = random.randint(-10, 10)
-
-        entity.move(xMove, yMove)
+        if entity.fitness >= 0:
+            entity.color = (0, entity.fitness / 100000, 0)
+        else:
+            entity.color = (0, 0, 0)
 
     # Event loop
     for event in pygame.event.get():
@@ -102,3 +126,27 @@ while True:
 
     # Update the screen
     pygame.display.flip()
+
+entities = []
+
+# Create the entities on the bottom left
+for i in range(100):
+    entities.append(Entity(SCREEN_WIDTH/4, SCREEN_HEIGHT-SCREEN_HEIGHT/4, (0, 0, 0)))
+
+def main():
+    global time_since_last_generation
+
+    while True:
+        while time_since_last_generation < 3:
+            updateScreen(entities)
+
+            # Move every entity randomly
+            for entity in entities:
+                xMove = random.choice([-1, 1])
+                yMove = random.choice([-1, 1])
+
+                entity.move(xMove * 5, yMove * 5)
+
+
+if __name__ == "__main__":
+    main()
